f084122d923ca8f1e6bbb3f7b866f11da85edc3f
tmanson <mansoninformatique@gmail.com>
2015-12-31 10:29:03 +0100

    Ajout bundle migration

--- app/AppKernel.php
+++ app/AppKernel.php
@@ -21,6 +21,7 @@ class AppKernel extends Kernel
             new CE\UserBundle\CEUserBundle(),
             new CE\DeviceBundle\CEDeviceBundle(),
             new CE\ReservationBundle\CEReservationBundle(),
+            new Doctrine\Bundle\MigrationsBundle\DoctrineMigrationsBundle()
         );
 
         if (in_array($this->getEnvironment(), array('dev', 'test'))) {
