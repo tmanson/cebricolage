1c9efcb36229e39bdb645ef6df585c7918393e0b
MANSON Tomy Ext SCE/OAB <tmanson.ext@orange.com>
2017-04-14 14:14:35 +0200

    correction script migration

--- app/DoctrineMigrations/Version20170411132223.php
+++ app/DoctrineMigrations/Version20170411132223.php
@@ -18,9 +18,9 @@ class Version20170411132223 extends AbstractMigration
         // this up() migration is auto-generated, please modify it to your needs
         $this->abortIf($this->connection->getDatabasePlatform()->getName() != 'mysql', 'Migration can only be executed safely on \'mysql\'.');
 
-        $this->addSql('ALTER TABLE history CHANGE date date DATETIME NOT NULL');
-        $this->addSql('ALTER TABLE reservation CHANGE status_id status_id INT NOT NULL, CHANGE device_id device_id INT NOT NULL, CHANGE user_id user_id INT NOT NULL');
-        $this->addSql('ALTER TABLE permanence CHANGE startDate startDate DATETIME NOT NULL, CHANGE endDate endDate DATETIME NOT NULL');
+        $this->addSql('ALTER TABLE History CHANGE date date DATETIME NOT NULL');
+        $this->addSql('ALTER TABLE Reservation CHANGE status_id status_id INT NOT NULL, CHANGE device_id device_id INT NOT NULL, CHANGE user_id user_id INT NOT NULL');
+        $this->addSql('ALTER TABLE Permanence CHANGE startDate startDate DATETIME NOT NULL, CHANGE endDate endDate DATETIME NOT NULL');
     }
 
     /**
