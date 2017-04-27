833d099780edb770915e6eab384036b8e40933ed
MANSON Tomy Ext SCE/OAB <tmanson.ext@orange.com>
2017-04-27 12:43:11 +0200

    Corection script migration

--- app/DoctrineMigrations/Version20170425121807.php
+++ app/DoctrineMigrations/Version20170425121807.php
@@ -18,9 +18,9 @@ class Version20170425121807 extends AbstractMigration
         // this up() migration is auto-generated, please modify it to your needs
         $this->abortIf($this->connection->getDatabasePlatform()->getName() != 'mysql', 'Migration can only be executed safely on \'mysql\'.');
 
-        $this->addSql('ALTER TABLE device CHANGE modele modele VARCHAR(255) DEFAULT NULL');
-        $this->addSql('ALTER TABLE history CHANGE date date DATETIME NOT NULL');
-        $this->addSql('ALTER TABLE permanence CHANGE startDate startDate DATETIME NOT NULL, CHANGE endDate endDate DATETIME NOT NULL');
+        $this->addSql('ALTER TABLE Device CHANGE modele modele VARCHAR(255) DEFAULT NULL');
+        $this->addSql('ALTER TABLE History CHANGE date date DATETIME NOT NULL');
+        $this->addSql('ALTER TABLE Permanence CHANGE startDate startDate DATETIME NOT NULL, CHANGE endDate endDate DATETIME NOT NULL');
     }
 
     /**
